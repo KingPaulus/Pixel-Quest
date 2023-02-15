00c751ea69e85802e919ec0ef20a3b2f1f4de479
King Paulus <mergetp02@gmail.com>
2023-02-15 13:45:53 +0100

    Add Texture

--- src/PixelQuest/Game.java
+++ src/PixelQuest/Game.java
@@ -4,27 +4,37 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Game extends JPanel implements KeyListener {
     private int playerX = 50;
     private int playerY = 50;
+    private BufferedImage playerTexture;
 
     public Game() {
-        JFrame frame = new JFrame("My PixelQuest.Game");
+        JFrame frame = new JFrame("My Game");
         frame.add(this);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
         frame.setVisible(true);
         frame.addKeyListener(this);
+
+        try {
+            playerTexture = ImageIO.read(new File("image/player.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void paint(Graphics g) {
         super.paint(g);
-        g.setColor(Color.BLUE);
-        g.fillRect(playerX, playerY, 50, 50);
+        g.drawImage(playerTexture, playerX, playerY, null);
     }
 
     public void keyPressed(KeyEvent e) {
